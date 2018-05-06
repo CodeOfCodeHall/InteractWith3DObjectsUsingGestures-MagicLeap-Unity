1fa121d6633c4f4992ae7ef9c5fc50593803747f
King <codeofcodehall@gmail.com>
2018-05-06 16:15:26 +0300

    Adding initial files
    - Added GestureDetection.cs sets up the Gestures to be tracked and tracks them
    - Added TouchCubeCollider.cs to handle touches (collisions) on the cube

---
+++ scripts/GestureDetection.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR.MagicLeap;
+public class GestureDetection : MonoBehaviour
+{
+    //game object representing the finger position
+    public Transform finger;
+    // Use this for initialization
+    void Start()
+    {
+        if (!MLHands.Start())
+        {
+            Debug.Log("MLHands didn't start...bail.");
+            return;
+        }
+        //set gestures to track
+        setGesturesToTrack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if MLHands is running start tracking gestures
+        if (MLHands.IsStarted)
+            gestureTracker();
+    }
+
+    //set the gestures to track
+    void setGesturesToTrack()
+    {
+        List<MLStaticGestureType> gestures = new List<MLStaticGestureType>();
+        //add the gestures we want to track
+        gestures.Add(MLStaticGestureType.Finger);
+        gestures.Add(MLStaticGestureType.L);
+        //add the gestures to the gesture manager
+        MLHands.GestureManager.EnableGestures(gestures.ToArray(), true, true);
+    }
+
+    //track the gestures
+    void gestureTracker()
+    {
+        //check for the l or finger gesture
+        //and that we've got some keypoints
+        if ((MLHands.Left.StaticGesture == MLStaticGestureType.Finger ||
+               MLHands.Left.StaticGesture == MLStaticGestureType.L) &&
+               MLHands.Left.KeyPoints.Length > 0)
+        {
+            positionHand(MLHands.Left);
+        }
+        //check for the l or finger gesture
+        //and that we've got some keypoints
+        if ((MLHands.Right.StaticGesture == MLStaticGestureType.Finger ||
+                MLHands.Right.StaticGesture == MLStaticGestureType.L) &&
+                MLHands.Right.KeyPoints.Length > 0)
+        {
+            positionHand(MLHands.Right);
+        }
+    }
+
+    //position the game object to the tracked finger
+    void positionHand(MLHand hand)
+    {
+        //in Lumin SDK 0.13 only the center hand position
+        //updates so we need to use that until it's fixed
+        finger.position = hand.Center;
+        //set the finger position to the tip of the finger
+        //finger.position = hand[1].position;
+    }
+
+    private void OnDestroy()
+    {
+        MLHands.Stop();
+    }
+}
---
+++ scripts/TouchCubeCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCubeCollider : MonoBehaviour
+{
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //change the colour of the cube
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
